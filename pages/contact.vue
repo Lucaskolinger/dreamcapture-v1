1934c689349ee039cf592cff1f5f83a6e911141b
Lucas Kolinger <lucaskolinger@icloud.com>
2022-03-26 00:24:05 +0100

    Contact page hover effects

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="container">
-    <div class="img">Bild Platzhalter</div>
+    <div ref="imgcontainer" class="img">Bild Platzhalter</div>
     <h2>Stay in touch!</h2>
-    <div class="form-container">
+    <div
+      class="form-container"
+      @mouseover="changePicture"
+      @mouseleave="changePictureAgain"
+    >
       <h1>Finn-Oliver Walter</h1>
       <div class="mail-row">
         <button ref="btn" @click="copyMailToClipboard">
@@ -68,7 +72,7 @@
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
-              d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
+              d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
             />
           </svg>
         </button>
@@ -102,6 +106,12 @@ export default {
         this.mailbtnAlerted = false;
       }, 1000);
     },
+    changePicture() {
+      this.$refs.imgcontainer.style.backgroundColor = "red";
+    },
+    changePictureAgain() {
+      this.$refs.imgcontainer.style.backgroundColor = "orange";
+    },
   },
 };
 </script>
@@ -125,7 +135,7 @@ button {
   background-color: $dc-blue;
 
   .img {
-    background-color: $dc-dark;
+    background-color: orange;
     aspect-ratio: 16 / 9;
     border-radius: 1rem;
     margin-inline: 1rem;
@@ -150,6 +160,12 @@ button {
     border-radius: 1rem;
     width: max-content;
     margin-inline: auto;
+
+    &:hover {
+      transform: translateY(-1rem) scale(105%);
+      margin-top: 4rem;
+      box-shadow: 1px 1px $dc-orange;
+    }
 
     h1 {
       margin-bottom: 1rem;
