59ee674f4f2731f327fa2607db6ad885c29750cb
Lucas Kolinger <lucaskolinger@icloud.com>
2022-03-28 00:17:16 +0200

    removing dist

--- dist/_nuxt/static/1648404609/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648404609",layout:"MainGrid",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"32162fcf"}}}}(null,"\u002F"));
