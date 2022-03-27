0a01c0eb56547b0684426a95d59d9841cecc25fe
Lucas Kolinger <lucaskolinger@icloud.com>
2022-03-27 20:13:13 +0200

    test2

---
+++ dist/_nuxt/static/1648404609/projekte/blaue-briefe/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648404609",layout:"MainGrid",error:a,serverRendered:true,routePath:"\u002Fprojekte\u002Fblaue-briefe",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"32162fcf"}}}}(null));
