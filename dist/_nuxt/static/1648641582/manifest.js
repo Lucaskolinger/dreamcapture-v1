2143c21d3c87db3d07e0a1cc70f504ae48b867ef
Lucas Kolinger <lucaskolinger@icloud.com>
2022-03-30 14:00:08 +0200

    forgot to generate for deployment

---
+++ dist/_nuxt/static/1648641582/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdatenschutz","\u002Fabout","\u002Fcontact","\u002Fimpressum","\u002Fprojekte\u002Fblaue-briefe","\u002Fprojekte\u002Fdlikw","\u002Fprojekte\u002F24htotake","\u002Fprojekte\u002Fdkmn","\u002Fprojekte\u002Ftboy","\u002Fprojekte\u002Fbicycle"]})
---
+++ dist/_nuxt/static/1648641582/projekte/24htotake/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648641582",layout:"MainGrid",error:a,serverRendered:true,routePath:"\u002Fprojekte\u002F24htotake",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"df3d44a5"}}}}(null));
