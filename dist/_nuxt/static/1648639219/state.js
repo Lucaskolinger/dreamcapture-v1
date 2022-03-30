c64bd98795a0d0524cbd3e01e2a17e0009b9f22d
Lucas Kolinger <lucaskolinger@icloud.com>
2022-03-30 13:20:46 +0200

    testing for deployment; Updating dist

---
+++ dist/_nuxt/static/1648639219/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648639219",layout:"MainGrid",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8d186bcb"}}}}(null,"\u002F"));
