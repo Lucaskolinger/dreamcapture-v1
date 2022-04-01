200a1800001f8a7262526947645ef1ac1a36278f
Lucas Kolinger <lucaskolinger@icloud.com>
2022-04-02 00:17:46 +0200

    forgot generating for test deploy

--- dist/_nuxt/static/1648677760/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648677760",layout:"MainGrid",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e9a5e39d"}}}}(null,"\u002F"));
---
+++ dist/_nuxt/static/1648851449/projekte/blaue-briefe/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648851449",layout:"MainGrid",error:a,serverRendered:true,routePath:"\u002Fprojekte\u002Fblaue-briefe",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"02b9419c"}}}}(null));
